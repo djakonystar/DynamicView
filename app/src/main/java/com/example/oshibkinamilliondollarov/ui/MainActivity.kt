cb28e14de39b55969867c8a738d22d4dfb6f5c7a
Isco-2211 <isco.jalgasbaev@gmail.com>
2021-05-27 19:30:26 +0500

    set up database

--- app/src/main/java/com/example/oshibkinamilliondollarov/ui/MainActivity.kt
+++ app/src/main/java/com/example/oshibkinamilliondollarov/ui/MainActivity.kt
@@ -13,6 +13,8 @@ import com.example.oshibkinamilliondollarov.ui.home.HomeFragment
 
 class MainActivity : AppCompatActivity() {
 
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
